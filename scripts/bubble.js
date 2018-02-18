354fdaac4bca6d7771f2c17220007904e3a99afb
wookieJ <lukaszjedryczka.biuro@gmail.com>
2018-02-18 21:51:32 +0100

    image drawing

--- scripts/bubble.js
+++ scripts/bubble.js
@@ -1,7 +1,13 @@
 const canvasElem = document.getElementById('canvas');
+const canvasElemPhoto = document.getElementById('photo');
 const ctx = canvasElem.getContext('2d');
+const ctxPhoto = canvasElemPhoto.getContext('2d');
 
 var circles = new Array();
+var img = new Image();
+img.src = "data/tak.png";
+
+var whiteArea = [];
 
 function Circle(x, y)
 {
@@ -12,17 +18,20 @@ function Circle(x, y)
 	
 	this.show = function()
 	{
-		ctx.beginPath();
-		ctx.arc(this.x, this.y, this.r, 0, 2*Math.PI);
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.r, 0, 2*Math.PI);
         ctx.fill();
         ctx.lineWidth = 2;
         ctx.strokeStyle = "#FFFFFF";
-		ctx.stroke();
+        ctx.stroke();
 	}
     
     this.grow = function()
     {
-        this.r++;
+        if(this.r < 30)
+            this.r++;
+        else
+            this.growing = false;
     }
 }
 
@@ -31,29 +40,68 @@ function distance(c1, c2)
     return Math.sqrt(Math.pow(c1.x - c2.x,2) + Math.pow(c1.y - c2.y,2));
 }
 
+function searchArray(array, x, y)
+{
+    for(i=0 ; i<array.length ; i++)
+    {
+        if(array[i][0] == x && array[i][1] == y)
+        {
+            return true;
+        }
+    }
+    
+    return false;
+}
+
 function setup()
 {
-    canvasElem.width = 800;
-    canvasElem.height = 500;
+    // canvas sizes
+    canvasElem.width = img.width;
+    canvasElem.height = img.height;
+    canvasElemPhoto.width = img.width;
+    canvasElemPhoto.height = img.height;
     canvasElem.style.backgroundColor = "black";
+    canvasElemPhoto.style.visibility = 'hidden';
+    
+    ctxPhoto.drawImage(img, 0, 0);
+    
+    // searching for white pixels and store their vector
+    for(i=0 ; i<img.width-1 ; i++)
+    {
+        for(j=0 ; j<img.height-1 ; j++)
+        {
+            var pixel = ctxPhoto.getImageData(i,j,1,1);
+            var data = pixel.data;
+            
+            if(data[0] > 1)
+            {
+                whiteArea.push([i,j]);
+            }
+        }
+    }
+    
+    console.log(whiteArea.length);
 }
 
 function run()
 {
-    x = Math.random() * canvasElem.width;
-    y = Math.random() * canvasElem.height;
-    
+    rand = Math.random() * whiteArea.length;
+    rand = Math.floor(rand);
+        
+    x = whiteArea[rand][0];
+    y = whiteArea[rand][1];
+        
     c = new Circle(x,y);
     circles.push(c);
     
     for(i=0 ; i<circles.length ; i++)
     {
-        
         if(circles[i].growing == true)
         { 
             // outside canvas
             if(circles[i].x-circles[i].r < 0 || circles[i].x+circles[i].r > canvasElem.width || circles[i].y+circles[i].r > canvasElem.height || circles[i].y-circles[i].r < 0)
                 circles[i].growing = false;
+            
             // check if not overlapping another
             for(j=0 ; j<circles.length ; j++)
             {
@@ -74,4 +122,4 @@ function run()
 }
 
 setup();
-setInterval(run,10);
+setInterval(run,1);
